9bf4ca857df573b4a7156bf8990b3fec19f9b1cd
danielf9403 <103592213+danielf9403@users.noreply.github.com>
2023-02-08 23:23:03 -0500

    update tv class

--- classTV.js
+++ classTV.js
@@ -1,6 +1,7 @@
 // serch channels
 // increase or decrease volume
 // ajust th contract
+// ajust brightness
 // turn on or off
 // imput source
 // ajust color
@@ -33,14 +34,64 @@ class TV extends CabeleChannels {
         this.brand = brand;
         this.resolution = resolution;
         this.color = color;
+        this.contract = 50;
+        this.brightness = 50; 
         this.volume = 20;
+        this.max = 100;
+        this.min = 0;
+        this.brightnessMax = 100;
+        this.brightnessMin = 0;
+        this.contractMax =100;
+        this.contractMin =0;
     }
 
     increaseVolume(){
-        if()
+        if(this.volume < this.max){
+            this.volume + 1;
+        }else{
+            console.log("Volume is set to max");
+        }
     }
 
-    decreaseVolume(){}
+    decreaseVolume(){
+        if(this.volume < this.min){
+            this.volume -= 1;
+        }else{
+            console.log("Volume is set to mute");
+        }
+    }
+
+    increaseContract(){
+        if(this.contract < this.contractMax){
+            this.contract + 1;
+        }else{
+            console.log("Contract is set to max");
+        }
+    }
+
+    decreaseContract(){
+        if(this.contract < this.contractMin){
+            this.contract -= 1;
+        }else{
+            console.log("Contract is set to min");
+        }
+    }
+
+    increaseBrightness(){
+        if(this.brightness < this.brightnessMax){
+            this.brightness + 1;
+        }else{
+            console.log("Brightness is set to max");
+        }
+    }
+
+    decreaseBrightness(){
+        if(this.brightness < this.brightnessMin){
+            this.brightness -= 1;
+        }else{
+            console.log("Brightness is set to min");
+        }
+    }
 
     configureChannelslToTv(){}
 }
